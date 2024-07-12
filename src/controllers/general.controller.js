3a3052326dada9328458e1749faf8ed244200715
padil <fadhilgani2@gmail.com>
2024-07-12 16:21:43 +0700

    add dummy data for freelance, products, and testimoni

--- src/controllers/general.controller.js
+++ src/controllers/general.controller.js
@@ -1,24 +1,60 @@
-import fetch from "node-fetch";
+// fetch data from API
+// import fetch from "node-fetch";
+
+// // home page
+// export const home = async (req, res) => {
+//   // fetch data products
+//   const resultProduct = await fetch("http://192.168.18.22:3000/product", {
+//     method: "GET",
+//   });
+//   const products = await resultProduct.json();
+
+//   // fetch data testimoni
+//   const resultTestimoni = await fetch("http://192.168.18.22:3000/testimoni", {
+//     method: "GET",
+//   });
+//   const testimoni = await resultTestimoni.json();
+
+// console.log(products)
+// console.log(testimoni)
+
+//   // render to home page
+//   res.render("pages/home/home.ejs", {
+//     products,
+//     testimoni,
+//     page: "home",
+//     title: "Home",
+//   },);
+// };
+
+
+// local json
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Mendefinisikan __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // home page
 export const home = async (req, res) => {
-  // fetch data products
-  const resultProduct = await fetch("http://192.168.18.22:3000/product", {
-    method: "GET",
-  });
-  const products = await resultProduct.json();
+  // fetch data products from local json
+  const productFilePath = path.join(__dirname, '../data/local-json/productsData.json');
+  const productData = fs.readFileSync(productFilePath);
+  const products = JSON.parse(productData);
 
-  // fetch data kata mereka
-  const resultTestimoni = await fetch("http://192.168.18.22:3000/testimoni", {
-    method: "GET",
-  });
-  const testimoni = await resultTestimoni.json();
+  // fetch data testimoni from local json
+  const testimoniFilePath = path.join(__dirname, '../data/local-json/testimoniData.json');
+  const testimoniData = fs.readFileSync(testimoniFilePath);
+  const testimoni = JSON.parse(testimoniData);
 
   // render to home page
-  res.render("pages/home/home.ejs", {
+  res.render('pages/home/home.ejs', {
     products,
     testimoni,
-    page: "home",
-    title: "Home",
-  },);
+    page: 'home',
+    title: 'Home',
+  });
 };
+
--- src/controllers/services.controller.js
+++ src/controllers/services.controller.js
@@ -22,18 +22,40 @@ const translateIn = (req, res) => {
   });
 };
 
-// sarang freelance page
-const sarangFreelance = async (req, res) => {
-  // fetch data products
-  const resultProject = await fetch("http://192.168.18.22:3000/project", {
-    method: "GET",
-  });
-  const project = await resultProject.json();
+// sarang freelance page (fetch data from API)
+// const sarangFreelance = async (req, res) => {
+//   // fetch data products
+//   const resultProject = await fetch("http://192.168.18.22:3000/project", {
+//     method: "GET",
+//   });
+//   const project = await resultProject.json();
 
-  res.render("pages/sarang-freelance/sarang-freelance.ejs", {
+//   res.render("pages/sarang-freelance/sarang-freelance.ejs", {
+//     project,
+//     page: "sarang-freelance",
+//     title: "Sarang Freelance",
+//   });
+// };
+
+// sarang freelance page (fetch data from local JSON)
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Mendefinisikan __filename dan __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sarangFreelance = async (req, res) => {
+  // Baca data project dari local JSON
+  const projectFilePath = path.join(__dirname, '../data/local-json/freelanceData.json');
+  const projectData = fs.readFileSync(projectFilePath);
+  const project = JSON.parse(projectData);
+
+  res.render('pages/sarang-freelance/sarang-freelance.ejs', {
     project,
-    page: "sarang-freelance",
-    title: "Sarang Freelance",
+    page: 'sarang-freelance',
+    title: 'Sarang Freelance',
   });
 };
 
